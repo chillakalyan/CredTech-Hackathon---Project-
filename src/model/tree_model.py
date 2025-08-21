7b7aa76d1907f451d9250f9836ec1f190ff385fb
Chilla Kalyan <127284726+chillakalyan@users.noreply.github.com>
2025-08-21 11:11:17 +0530

    Update tree_model.py

--- src/model/tree_model.py
+++ src/model/tree_model.py
@@ -1,1 +1,25 @@
+# tree_model.py
+# Optional decision tree + SHAP utils
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+import shap
+
+class TreeModel:
+    def __init__(self, max_depth=3):
+        self.model = DecisionTreeClassifier(max_depth=max_depth)
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        self.model.fit(X, y)
+
+    def predict(self, X: pd.DataFrame):
+        return self.model.predict(X)
+
+    def explain(self, X: pd.DataFrame):
+        """
+        Generate SHAP explanations.
+        """
+        explainer = shap.TreeExplainer(self.model)
+        shap_values = explainer.shap_values(X)
+        return shap_values
 
